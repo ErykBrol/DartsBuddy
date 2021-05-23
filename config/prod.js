02727c0bb3844d57fa2e813d9ced35338af932c8
Eryk Brol <brol.eryk@gmail.com>
2021-05-22 20:07:09 -0400

    Add passportjs to the express app

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
    port: process.env.PORT,
+   cookieKey: process.env.COOKIE_KEY,
 };
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const path = require('path');
 
 /* Import config */
@@ -15,6 +17,14 @@ const gameRoutes = require('./routes/gameRoutes');
 
 /* Middleware */
 app.use(express.json());
+app.use(
+   cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKey],
+   })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* Route middleware */
 app.use('/auth', authRoutes);
